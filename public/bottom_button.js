381f68b8f8e1198047b1d4daa3d5e2615403a46e
Amin <499888@student.fontys.nl>
2023-05-24 23:39:15 +0200

    added multiple scss files in order to increase responsiveness of the website

---
+++ public/bottom_button.js
@@ -0,0 +1,26 @@
+document.addEventListener('scroll', handleScroll);
+var bottomButton = document.querySelector('.bottom_button');
+
+function handleScroll() {
+  var scrollableHeight =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  var GOLDEN_RATIO = 0.3;
+
+  if (document.documentElement.scrollTop / scrollableHeight > GOLDEN_RATIO) {
+    //show button
+    bottomButton.style.display = 'flex';
+  } else {
+    //hide button
+    bottomButton.style.display = 'none';
+  }
+}
+
+// bottomButton.addEventListener('click', scrollToTop);
+
+// function scrollToTop() {
+//   window.scrollTo({
+//     top: 0,
+//     behavior: 'smooth',
+//   });
+// }
