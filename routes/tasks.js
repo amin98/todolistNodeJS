18fc0e72943b384440995d2fa7b1725f8401382f
Amin <499888@student.fontys.nl>
2023-05-24 22:02:31 +0200

    primary requirements for project are functional

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const Task = require('./../models/task');
+const task = require('./../models/task');
 const router = express.Router();
 var app = express();
 
@@ -8,39 +9,56 @@ router.get('/new', (req, res) => {
 });
 
 router.get('/edit/:id', async (req, res) => {
-  const task = await Task.findById(req.params.id)
+  const task = await Task.findById(req.params.id);
   res.render('tasks/edit', { task: task });
 });
 
 router.get('/:slug', async (req, res) => {
   // res.send(req.params.id);
-  const task = await Task.findOne({slug: req.params.slug});
-  
-  if (task == null) res.redirect('/') 
+  const task = await Task.findOne({ slug: req.params.slug });
+
+  if (task == null) res.redirect('/');
   res.render('tasks/show', { task: task });
 });
 
-router.post('/', async (req, res) => {
-  // console.log(req.body);
+router.post(
+  '/',
+  async (req, res, next) => {
+    // console.log(req.body);
+    req.task = new Task();
+    next();
+  },
+  saveTaskAndRedirect('new')
+);
 
-  let task = new Task({
-    name: req.body.name,
-    description: req.body.description,
-    priority: req.body.priority,
-    duedate: req.body.duedate,
-  });
-  try {
-    task = await task.save();
-    res.redirect(`/tasks/${task.slug}`);
-  } catch (e) {
-    console.log(e);
-    res.render('tasks/new', { task: task });
-  }
+router.put(
+  '/:id',
+  async (req, res, next) => {
+    // console.log(req.body);
+    req.task = await task.findById(req.params.id)
+    next();
+  },
+  saveTaskAndRedirect('edit')
+);
+router.delete('/:id', async (req, res) => {
+  await Task.findByIdAndDelete(req.params.id);
+  res.redirect('/');
 });
 
-router.delete("/:id", async (req, res)=>{
-  await Task.findByIdAndDelete(req.params.id)
-  res.redirect('/')
-})
+function saveTaskAndRedirect(path) {
+  return async (req, res) => {
+    let task = req.task;
+    task.name = req.body.name;
+    task.description = req.body.description;
+    task.priority = req.body.priority;
+    task.duedate = req.body.duedate;
 
+    try {
+      task = await task.save();
+      res.redirect(`/tasks/${task.slug}`);
+    } catch (e) {
+      res.render(`tasks/${path}`, { task: task });
+    }
+  };
+}
 module.exports = router;
